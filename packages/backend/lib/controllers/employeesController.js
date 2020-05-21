4be952ae04c04e6c761102d7590141c86312cbe2
Kris Bastiani <kris.bastiani@gmail.com>
2020-05-21 16:18:08 +1000

    backend: clean db data on get

--- packages/backend/lib/controllers/employeesController.js
+++ packages/backend/lib/controllers/employeesController.js
@@ -2,5 +2,14 @@ const mongoose = require('mongoose');
 
 const Employee = mongoose.model('Employees');
 
-exports.getEmployees = (request, response) => Employee
-	.find({}, (err, employee) => (err ? response.send(err) : response.json(employee)));
+exports.getEmployees = (request, response) => Employee.find({}, (err, data) => {
+	if (err) return response.send(err);
+
+	const employees = data.map(entry => ({
+		id: entry.ID,
+		managerId: entry['Manager ID'],
+		name: entry['Employee Name'],
+	}));
+
+	return response.json(employees);
+});
