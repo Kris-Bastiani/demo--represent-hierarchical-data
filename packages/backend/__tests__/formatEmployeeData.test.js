7b7d9ca11c9fb9a4534c0fe800c8dfbb4556fe0b
Kris Bastiani <kris.bastiani@gmail.com>
2020-05-21 19:51:02 +1000

    backend: add unit test for formatEmployeeData

---
+++ packages/backend/__tests__/formatEmployeeData.test.js
@@ -0,0 +1,8 @@
+const mockEmployeesFormatted = require('../__mocks__/employeesFormatted');
+const mockEmployeesRaw = require('../__mocks__/employeesRaw');
+
+const formatEmployeeData = require('../lib/helpers/formatEmployeeData');
+
+test('Returns expected data', () => {
+	expect(formatEmployeeData(mockEmployeesRaw)).toEqual(mockEmployeesFormatted);
+});
