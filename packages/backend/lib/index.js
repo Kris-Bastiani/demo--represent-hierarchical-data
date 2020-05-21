a864bc552dc99a018baa9b114d86e05e0c7592a5
Kris Bastiani <kris.bastiani@gmail.com>
2020-05-21 12:30:22 +1000

    backend: set up express app

--- packages/backend/lib/index.js
+++ packages/backend/lib/index.js
@@ -1,7 +1,21 @@
-'use strict';
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
 
-module.exports = backend;
+const PORT = process.env.PORT || 3002;
 
-function backend() {
-	// TODO
-}
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Employeedb');
+
+const app = express();
+app.use(helmet());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors({
+	origin: 'http://localhost:3000',
+	optionsSuccessStatus: 200,
+}));
+
+app.listen(PORT);
