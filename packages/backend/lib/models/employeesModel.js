d71bcc186814997e5968b3e464b2f96946039699
Kris Bastiani <kris.bastiani@gmail.com>
2020-05-21 15:55:18 +1000

    backend: Add seed for database

--- packages/backend/lib/models/employeesModel.js
+++ packages/backend/lib/models/employeesModel.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const EmployeeSchema = new Schema({
-	id: { type: Number },
-	managerId: { type: Number },
-	name: { type: String },
-});
+const EmployeeSchema = new Schema(
+	{
+		id: { type: Number },
+		managerId: { type: Number },
+		name: { type: String },
+	},
+	{ collection: 'employees' },
+);
 
 module.exports = mongoose.model('Employees', EmployeeSchema);
---
+++ seed.js
@@ -0,0 +1,7 @@
+const DB_PATH = 'localhost/momenton_test__kris_bastiani';
+
+module.exports = {
+	undefined: DB_PATH,
+	dev: DB_PATH,
+	prod: DB_PATH,
+};
