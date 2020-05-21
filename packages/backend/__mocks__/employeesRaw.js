73b8a530f04d3c4cea5e4956a7ab775422778056
Kris Bastiani <kris.bastiani@gmail.com>
2020-05-21 19:50:40 +1000

    backend: add employeesRaw mock data

---
+++ packages/backend/__mocks__/employeesRaw.js
@@ -0,0 +1,38 @@
+module.exports = [
+	{
+		_id: 'j110',
+		'Employee Name': 'Alan',
+		ID: 100,
+		'Manager ID': 150,
+	},
+	{
+		_id: 'j111',
+		'Employee Name': 'Martin',
+		ID: 220,
+		'Manager ID': 100,
+	},
+	{
+		_id: 'j112',
+		'Employee Name': 'Jamie',
+		ID: 150,
+		'Manager ID': null,
+	},
+	{
+		_id: 'j113',
+		'Employee Name': 'Alex',
+		ID: 275,
+		'Manager ID': 100,
+	},
+	{
+		_id: 'j114',
+		'Employee Name': 'Steve',
+		ID: 400,
+		'Manager ID': 150,
+	},
+	{
+		_id: 'j115',
+		'Employee Name': 'David',
+		ID: 190,
+		'Manager ID': 400,
+	},
+];
