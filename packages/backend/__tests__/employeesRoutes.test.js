52a0333ad1da29dd8b848c586d7ef0dd66c50038
Kris Bastiani <kris.bastiani@gmail.com>
2020-05-30 10:14:01 +1000

    backend: delete resolved TODO

--- packages/backend/__tests__/employeesRoutes.test.js
+++ packages/backend/__tests__/employeesRoutes.test.js
@@ -4,11 +4,6 @@ const createExpressServer = require('../lib/helpers/createExpressServer');
 
 const server = createExpressServer();
 
-// TODO: troubleshoot this error:
-// A worker process has failed to exit gracefully and has been force exited.
-// This is likely caused by tests leaking due to improper teardown.
-// Try running with --runInBand --detectOpenHandles to find leaks.
-
 describe('/employees route', () => {
 	test('Responds with status 200 on GET when there is a db connection', async () => {
 		await createDbConnection();
