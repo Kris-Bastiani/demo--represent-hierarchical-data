a0f338f9a0beb17fbafefe983a07ab44f66129e8
Kris Bastiani <kris.bastiani@gmail.com>
2020-05-30 10:20:07 +1000

    backend: mute db disconnection console.error in route test

--- packages/backend/__tests__/employeesRoutes.test.js
+++ packages/backend/__tests__/employeesRoutes.test.js
@@ -6,10 +6,15 @@ const server = createExpressServer();
 
 describe('/employees route', () => {
 	test('Responds with status 200 on GET when there is a db connection', async () => {
+		const spy = jest.spyOn(console, 'error');
+		spy.mockImplementation(() => {});
 		await createDbConnection();
 		const response = await request(server).get('/employees');
+
 		expect(response.statusCode).toEqual(200);
+
 		closeDbConnection();
+		spy.mockRestore();
 	});
 
 	test('Responds with status 500 on GET when there is no db connection', async () => {
