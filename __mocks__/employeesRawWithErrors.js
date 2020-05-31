a3038e512ba32d21d52db016cef52e102462b485
Kris Bastiani <kris.bastiani@gmail.com>
2020-05-31 16:58:17 +1000

    Add mock raw data with errors

---
+++ __mocks__/employeesRawWithErrors.js
@@ -0,0 +1,61 @@
+module.exports = [
+	{
+		_id: 'j110',
+		'Employee Name': 'Alan',
+		ID: 100,
+		'Manager ID': 150,
+	},
+	{
+		_id: 'j111',
+		'Employee Name': 'Martin',
+		ID: 220,
+		'Manager ID': 100,
+	},
+	{
+		_id: 'j112',
+		'Employee Name': 'Jamie',
+		ID: 150,
+		'Manager ID': null,
+	},
+	{
+		_id: 'j113',
+		'Employee Name': 'Alex',
+		ID: 275,
+		'Manager ID': 100,
+	},
+	{
+		_id: 'j114',
+		'Employee Name': 'Steve',
+		ID: 400,
+		'Manager ID': 150,
+	},
+	{
+		_id: 'j115',
+		'Employee Name': 'David',
+		ID: 190,
+		'Manager ID': 400,
+	},
+	{
+		_id: 'j116',
+		ID: 300,
+		'Manager ID': null,
+		'Employee Name': 'Foo',
+	},
+	{
+		_id: 'j117',
+		ID: 310,
+		'Manager ID': 900,
+		'Employee Name': 'Bar',
+	},
+	{
+		_id: 'j118',
+		ID: 320,
+		'Manager ID': 'Foo',
+		'Employee Name': 'Baz',
+	},
+	{
+		_id: 'j119',
+		ID: 330,
+		'Employee Name': 'Qux',
+	},
+];
