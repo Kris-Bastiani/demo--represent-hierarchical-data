e83d298a648b6a2d874cbb0a4cdb459a74d9c9e0
Kris Bastiani <kris.bastiani@gmail.com>
2020-05-31 11:41:50 +1000

    Add mock data for employees with errors

---
+++ __mocks__/employeesWithErrors.js
@@ -0,0 +1,51 @@
+module.exports = [
+	{
+		id: 100,
+		managerId: 150,
+		name: 'Alan',
+	},
+	{
+		id: 220,
+		managerId: 100,
+		name: 'Martin',
+	},
+	{
+		id: 150,
+		managerId: null,
+		name: 'Jamie',
+	},
+	{
+		id: 275,
+		managerId: 100,
+		name: 'Alex',
+	},
+	{
+		id: 400,
+		managerId: 150,
+		name: 'Steve',
+	},
+	{
+		id: 190,
+		managerId: 400,
+		name: 'David',
+	},
+	{
+		id: 300,
+		managerId: null,
+		name: 'Foo',
+	},
+	{
+		id: 310,
+		managerId: 900,
+		name: 'Bar',
+	},
+	{
+		id: 320,
+		managerId: 'Foo',
+		name: 'Baz',
+	},
+	{
+		id: 330,
+		name: 'Qux',
+	},
+];
