bebb7b45cb858ff3cd6d5c7814bfb478a5905647
Kris Bastiani <kris.bastiani@gmail.com>
2020-05-31 11:43:07 +1000

    backend: Check for invalid managers when formatting employee data

--- packages/backend/lib/helpers/formatEmployeeData.js
+++ packages/backend/lib/helpers/formatEmployeeData.js
@@ -1,5 +1,14 @@
-module.exports = data => data.map(entry => ({
-	id: entry.ID,
-	managerId: entry['Manager ID'],
-	name: entry['Employee Name'],
-}));
+const findInvalidParents = require('./findInvalidParents');
+
+module.exports = data => {
+	const formattedData = data.map(entry => ({
+		id: entry.ID,
+		managerId: entry['Manager ID'],
+		name: entry['Employee Name'],
+	}));
+
+	const invalidManagers = findInvalidParents(formattedData, 'managerId');
+	if (invalidManagers.length) console.error('Employees found with invalid managers:', invalidManagers);
+
+	return formattedData;
+};
