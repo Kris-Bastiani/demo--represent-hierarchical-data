1fa477d0618b88aa134c339788619e0f0ce0a768
Kris Bastiani <kris.bastiani@gmail.com>
2020-05-31 15:06:26 +1000

    frontend: render employee hierarchy as nested list

--- packages/frontend/src/assets/js/index.js
+++ packages/frontend/src/assets/js/index.js
@@ -1,4 +1,11 @@
+import { h, render } from 'preact';
+
 import getEmployees from './helpers/getEmployees';
 import sortByHierarchy from './helpers/sortByHierarchy';
+import NestedList from './components/atoms/NestedList';
 
-getEmployees().then(employees => console.log(sortByHierarchy(employees, 'managerId')));
+const elRoot = document.getElementById('js__app');
+
+getEmployees().then(employees => {
+	render(<NestedList items={sortByHierarchy(employees, 'managerId')} />, elRoot);
+});
